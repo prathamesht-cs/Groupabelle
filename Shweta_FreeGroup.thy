3c58ea3fbbe39016839472821fa7f9ec1608a5c1
Shweta-tkcs <78309333+Shweta-tkcs@users.noreply.github.com>
2021-07-14 20:16:02 +0530

    Reduction function properties.

---
+++ Shweta_FreeGroup.thy
@@ -0,0 +1,154 @@
+theory Shweta_FreeGroup
+imports Main  "HOL-Library.FuncSet" "HOL-Algebra.Group"
+begin
+
+(*From Generators.thy*)
+datatype ('a,'b) monoidgentype = C 'a  'b (infix "!" 63)
+
+datatype ('a,'b) groupgentype = P "('a,'b) monoidgentype"
+                               | N  "('a,'b) monoidgentype"
+
+type_synonym ('a,'b) word = "(('a,'b) groupgentype) list"
+
+primrec inverse::"('a,'b) groupgentype \<Rightarrow> ('a,'b) groupgentype"
+  where
+"inverse (P x) = (N x)"
+|"inverse (N x) = (P x)"
+
+fun reduction:: "('a,'b) word \<Rightarrow> ('a,'b) word"
+ where
+"reduction [] = []"
+|"reduction [x] = [x]"
+|"reduction (g1#g2#wrd) = (if (g1 = inverse g2) 
+                             then reduction wrd 
+                             else (g1#(reduction (g2#wrd))))"
+
+fun reduced::"('a,'b) word \<Rightarrow> bool"
+  where
+"reduced [] = True"
+|"reduced [g] = True"
+|"reduced (g#h#wrd) = (if (g \<noteq> inverse h) then reduced (h#wrd) else False)"
+
+primrec iter::"nat \<Rightarrow> ('a \<Rightarrow> 'a) \<Rightarrow> ('a \<Rightarrow> 'a)"
+  where
+"iter 0 f = (\<lambda> x. x)"
+|"iter (Suc n) f = (\<lambda> x. f ((iter n f) x))"
+
+lemma length_reduction : "length (reduction wrd) \<le> length wrd"
+proof(induction wrd rule: reduction.induct)
+case 1
+  then show ?case by simp
+next
+case (2 x)
+  then show ?case by simp
+next
+  case (3 g1 g2 wrd)
+  then show ?case 
+  proof(cases "g1 = inverse g2")
+    case True 
+    then show ?thesis using 3 by force
+  next
+    case False
+    then show ?thesis using 3 
+    by auto 
+ qed  
+qed
+
+lemma decreasing_length:
+  assumes "reduction wrd \<noteq> wrd"
+  shows "length (reduction wrd) < length wrd"
+  using assms
+proof(induction wrd rule: reduction.induct)
+case 1
+  then show ?case by simp
+next
+  case (2 x)
+  then show ?case by simp
+next
+  case (3 g1 g2 wrd)
+  then show ?case 
+  proof(cases "g1 = inverse g2")
+    case True
+    then have red_inv:"reduction (g1#g2#wrd) = reduction wrd" by auto
+    then show ?thesis 
+    proof(cases "reduction wrd = wrd")
+      case True
+      then have "reduction (g1#g2#wrd) = wrd" using red_inv by auto
+      then have "length (reduction (g1#g2#wrd)) = length wrd" by auto    
+      then show ?thesis 
+        by simp
+    next
+      case False
+      then have "length (reduction wrd) < length wrd" using 3 True by argo
+      then show ?thesis using red_inv by force
+    qed
+  next
+    case False
+    have prem:"reduction (g1#g2#wrd) \<noteq> (g1#g2#wrd)" using 3 by argo
+    then have "reduction (g1#g2#wrd) = g1#reduction (g2#wrd)" using False by auto
+    then have "reduction (g2#wrd) \<noteq> g2#wrd" using prem by fastforce
+    then have "length (g2#wrd) > length (reduction (g2#wrd))" using 3 False by blast
+    then have "length (g1#g2#wrd) > length (reduction (g1#g2#wrd))" using False by force
+    then show ?thesis by fast
+  qed  
+qed
+
+(*Prove 1. length (reduction (wrd)) = length wrd \<Rightarrow> reduction wrd = wrd*)
+lemma assumes "length (reduction (wrd)) = length wrd"
+  shows "reduction wrd = wrd"
+  using assms
+proof(induction wrd rule: reduction.induct)
+case 1
+  then show ?case 
+    by simp 
+next
+  case (2 x)
+  then show ?case by simp
+next
+case (3 g1 g2 wrd)
+  then show ?case
+  proof(cases "g1 = inverse g2")
+    case True
+    then have 1: "reduction (g1#g2#wrd) = reduction (wrd)" by simp     
+    then have "length (reduction (g1#g2#wrd)) = length (reduction (wrd))" 
+      by auto
+    moreover have "length (wrd) > length (reduction wrd)" using  1  
+    by (metis "3.prems" decreasing_length impossible_Cons nat_le_linear) (*"m \<le> n \<or> n \<le> m", "length xs \<le> length ys \<Longrightarrow> xs = x # ys = False"*)
+    then show ?thesis using 3 by auto
+  next
+    case False
+    then show ?thesis 
+    using  decreasing_length by blast
+  qed
+qed
+(*2.reduction wrd = wrd \<Rightarrow> reduced wrd*)
+lemma assumes "reduction wrd = wrd"
+  shows "reduced wrd"
+  using assms
+proof(induction wrd rule:reduction.induct)
+case 1
+then show ?case by simp
+next
+case (2 x)
+  then show ?case by simp
+next
+  case (3 g1 g2 wrd)
+  then show ?case 
+  proof(cases "g1 \<noteq> inverse g2")
+    case True
+     then show ?thesis using 3 by force   
+    next
+      case False
+     have "g1 = inverse g2"using False by auto
+     then have "(g1#g2#wrd) = reduction wrd" using 3 
+       by simp    
+     moreover then have "length (reduction(wrd)) \<le> length wrd"
+       by (simp add: length_reduction)
+     moreover then have "length (g1#g2#wrd) > length wrd"
+       by simp
+     ultimately show ?thesis  by simp 
+   qed
+qed
+(*3. lemma assumes length wrd = n shows iter (n+k) reduction = iter n reduction*)
+lemma assumes "length wrd = n" 
+  shows " iter (n+k) reduction = iter n reduction"
